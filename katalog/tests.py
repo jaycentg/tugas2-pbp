4fe274b0f872c6298656bf22e031adfff935977b
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2022-09-12 13:14:46 +0700

    initial commit

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from katalog.models import CatalogItem
+from django.urls import reverse, resolve
+from katalog.views import show_catalog
 
-# Create your tests here.
+class Testing(TestCase):
+    def test_model(self):
+        item = CatalogItem.objects.create(
+            item_name = "Fitbit Versa 3 [FB511BKBK-FRCJK] - Black",
+            item_price = 3699000,
+            item_stock = 1,
+            description = "SmartTrack yang secara otomatis merekam kegiatan berolahraga Anda",
+            rating = 5,
+            item_url = "https://www.tokopedia.com/fitbit-official/fitbit-versa-3-fb511bkbk-frcjk-black"
+        )
+        self.assertEqual(CatalogItem.objects.get(item_name = "Fitbit Versa 3 [FB511BKBK-FRCJK] - Black").item_name, \
+            "Fitbit Versa 3 [FB511BKBK-FRCJK] - Black")
+        print("Is instance:", isinstance(item, CatalogItem))
+        self.assertTrue(isinstance(item, CatalogItem))
+    
+    def test_url(self):
+        url = reverse("katalog:show_catalog")
+        print("Resolve:", resolve(url))
+        self.assertEquals(resolve(url).func, show_catalog)
+    
+    def test_view(self):
+        client = Client()
+        response = client.get(reverse("katalog:show_catalog"))
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "katalog.html")
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_item': data_catalog_item,
+    'nama': 'Jaycent Gunawan Ongris'
+}
+
+def show_catalog(request):
+    return render(request, "katalog.html", context)
