49ba3589c5b93f595e57f54c2ee1cbc6bae02fbb
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2022-09-13 14:03:34 +0700

    moving data_catalog_item and context into function's body

--- katalog/views.py
+++ katalog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
-data_catalog_item = CatalogItem.objects.all()
-context = {
-    'list_item': data_catalog_item,
-    'nama': 'Jaycent Gunawan Ongris',
-    'npm': '2106750231'
-}
-
 def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_catalog_item,
+        'nama': 'Jaycent Gunawan Ongris',
+        'npm': '2106750231'
+    }
+    
     return render(request, "katalog.html", context)
